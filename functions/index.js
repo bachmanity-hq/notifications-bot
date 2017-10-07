8274b28885daf70fac202724aee74bbfb12077a2
Dan Conger <dconger2@gmail.com>
2017-10-07 12:39:11 -0600

    update

--- functions/index.js
+++ functions/index.js
@@ -16,14 +16,15 @@ app.use(bodyParser.json());
  * Used to notify app of new helpdesk tickets in this case
  */
 app.post('/incoming', (req, res) => {
-  Ticket.fromExternal(req.body)
-    .then((ticket) => ticket.postToChannel())
-    .then(() => {
-      res.sendStatus(200);
-    })
-    .catch((error) => {
-      res.status(400).send('The ticket was not created');
-    });
+  res.status(200).send('hello dan');
+  // Ticket.fromExternal(req.body)
+  //   .then((ticket) => ticket.postToChannel())
+  //   .then(() => {
+  //     res.sendStatus(200);
+  //   })
+  //   .catch((error) => {
+  //     res.status(400).send('The ticket was not created');
+  //   });
 });
 
 /*
@@ -32,39 +33,43 @@ app.post('/incoming', (req, res) => {
  * 3rd-party system based on the action taken in Slack.
  */
 app.post('/interactive-message', (req, res) => {
-  const {
-      token,
-      actions,
-      callback_id,
-      response_url,
-      user
-  } = JSON.parse(req.body.payload);
-
-  const { verification_token } = functions.config().slack.bachmanity.conversations;
-
-  if (token === verification_token) {
-    // Immediately respond to signal success, further updates will be done using `response_url`
-    res.send('');
-
-    Ticket.find(callback_id).then((ticket) => {
-      debug('interactive message ticket found');
-      const action = actions[0];
-      const fieldName = action.name;
-      // Handle either message menu action or message button (Claim)
-      const fieldValue = action.selected_options ? action.selected_options[0].value : user.id;
-
-      return ticket.updateField(fieldName, fieldValue).then(() => {
-        debug('updating notification in channel');
-        return ticket.postToChannel(response_url);
-      });
-    })
-    .catch(console.error);
-  } else {
-    res.sendStatus(404);
-  }
+  // const {
+  //     token,
+  //     actions,
+  //     callback_id,
+  //     response_url,
+  //     user
+  // } = JSON.parse(req.body.payload);
+  //
+  // const { verification_token } = functions.config().slack.bachmanity.conversations;
+  //
+  // if (token === verification_token) {
+  //   // Immediately respond to signal success, further updates will be done using `response_url`
+  //   res.send('');
+  //
+  //   Ticket.find(callback_id).then((ticket) => {
+  //     debug('interactive message ticket found');
+  //     const action = actions[0];
+  //     const fieldName = action.name;
+  //     // Handle either message menu action or message button (Claim)
+  //     const fieldValue = action.selected_options ? action.selected_options[0].value : user.id;
+  //
+  //     return ticket.updateField(fieldName, fieldValue).then(() => {
+  //       debug('updating notification in channel');
+  //       return ticket.postToChannel(response_url);
+  //     });
+  //   })
+  //   .catch(console.error);
+  // } else {
+  //   res.sendStatus(404);
+  // }
 });
 
-exports.notifications = functions.https.onRequest((req, res) => {
-      if (!req.path) req.url = `/${req.url}`
-      return app(req, res)
+app.listen( process.env.PORT || 3000, function(){
+  console.log('Listening on port 3000');
 });
+
+// exports.notifications = functions.https.onRequest((req, res) => {
+//       if (!req.path) req.url = `/${req.url}`
+//       return app(req, res)
+// });
