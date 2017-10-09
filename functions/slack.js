fd706dad8c88ff0c340793ba4b379744f298359d
Jake Hasler <jakehasler@gmail.com>
2017-10-09 15:01:36 -0600

    adding linting + tests basics

--- functions/slack.js
+++ functions/slack.js
@@ -1,31 +1,24 @@
-'use strict';
 const axios = require('axios');
 const template = require('./template');
-const qs = require('querystring');
 const _ = require('lodash');
 const functions = require('firebase-functions');
 
 class Slack {
   constructor(props) {
-    for (let prop in props) {
-      if (props.hasOwnProperty(prop)) {
-        this[prop] = props[prop];
-      }
-    }
+    _.forOwn(props, (v, k) => {
+      this[k] = v;
+    });
   }
 
   send() {
     const {
-      client_id,
-      client_secret,
-      verification_token,
       webhook_url,
       oauth_access_token
     } = _.get(this.config, 'bachmanity.coup_notifications');
 
     const body = {
-      title: this.title,
-      text: this.message,
+      title: this.type,
+      text: this.title,
       token: oauth_access_token,
       channel: '#notifications'
     };
@@ -33,12 +26,11 @@ class Slack {
     return axios.post(webhook_url, body);
   }
 
-  static sendNotification({ title, message }) {
+  static sendNotification({ type, title, meta }) {
     const config = functions.config().slack;
-    const slack = new Slack({ config, title, message });
+    const slack = new Slack({ config, type, title, meta });
     return slack.send();
   }
-
 }
 
 module.exports.Slack = Slack;
